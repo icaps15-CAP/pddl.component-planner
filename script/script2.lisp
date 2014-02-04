d4f71643ec161a23d39cee0360b63d635bf800c4
Masataro Asai <guicho2.71828@gmail.com>
2014-02-05 02:37:10 +0900

    do not exit

--- script/script2.lisp
+++ script/script2.lisp
@@ -5,8 +5,6 @@
 (in-package :pddl.component-planner-test)
 
 (defun run-benchmark (i)
-  (handler-bind ((error (lambda (c)
-                          (sb-ext:exit))))
-    (benchmark i)
-    (print "finished successfully.")))
+  (benchmark i)
+  (print "finished successfully."))
 
