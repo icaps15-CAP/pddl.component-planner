f11d001d21c3d23da08c61840e206bd1fd63efe2
Masataro Asai <guicho2.71828@gmail.com>
2014-02-05 00:58:49 +0900

    cl-user

--- script/script2.lisp
+++ script/script2.lisp
@@ -4,7 +4,7 @@
 (asdf:load-system :pddl.component-planner-test)
 (in-package :pddl.component-planner-test)
 
-(defun run-benchmark (i)
+(defun cl-user:run-benchmark (i)
   (handler-bind ((error (lambda (c)
                           (sb-ext:exit))))
     (benchmark i)))
