8f7d5175f717ca292ae0add55ad66b84bbc714d9
Masataro Asai <guicho2.71828@gmail.com>
2014-03-03 01:30:14 +0900

    the large data are loaded only in script2.lisp

--- script/script2.lisp
+++ script/script2.lisp
@@ -4,6 +4,37 @@
 (asdf:load-system :pddl.component-planner-test)
 (in-package :pddl.component-planner-test)
 
+(defparameter *delayed-problems*
+              (list (delay (load-and-collect-problems
+                            '(:pddl.instances.barman-x1.3)
+                            '(:cocktail :shot)
+                            ".*BARMAN-.*"))
+                    (delay (load-and-collect-problems
+                            '(:pddl.instances.cell-assembly-eachparts)
+                            '(:base)
+                            "CELL-ASSEMBLY-(2A2B-MIXED-EACH-.*|MODEL2A-EACH-[12][0-9])"))
+                    (delay (load-and-collect-problems
+                            '(:pddl.instances.elevators)
+                            '(:passenger)
+                            ".*ELEVATORS.*"))
+                    (delay (load-and-collect-problems
+                            '(:pddl.instances.openstacks)
+                            '(:order :product)
+                            ".*OPENSTACKS.*"))
+                    (delay (load-and-collect-problems
+                            '(:pddl.instances.rover)
+                            '(:objective)
+                            "ROVERPROB[0-9]*"))
+                    (delay (load-and-collect-problems
+                            '(:pddl.instances.satellite-typed)
+                            '(:direction)
+                            "SATELLITE-TYPED-.*"))
+                    (delay (load-and-collect-problems
+                            '(:pddl.instances.woodworking-large
+                              :pddl.instances.woodworking-xlarge)
+                            '(:part)
+                            "WOOD-PROB-SAT-[0-9]*"))))
+
 (defun run-benchmark (i)
   (format t "running ~a th domain in script2.lisp" i)
   (handler-bind ((storage-condition (lambda (c)
--- t/pddl.component-planner.lisp
+++ t/pddl.component-planner.lisp
@@ -50,36 +50,11 @@
              (mapcar (curry #'list x) seeds))
            (collect-problems regexp)))
 
-(defvar *delayed-problems*
-    (list (delay (load-and-collect-problems
-                  '(:pddl.instances.barman-x1.3)
-                  '(:cocktail :shot)
-                  ".*BARMAN-.*"))
-          (delay (load-and-collect-problems
-                  '(:pddl.instances.cell-assembly-eachparts)
-                  '(:base)
-                  "CELL-ASSEMBLY-(2A2B-MIXED-EACH-.*|MODEL2A-EACH-[12][0-9])"))
-          (delay (load-and-collect-problems
-                  '(:pddl.instances.elevators)
-                  '(:passenger)
-                  ".*ELEVATORS.*"))
-          (delay (load-and-collect-problems
-                  '(:pddl.instances.openstacks)
-                  '(:order :product)
-                  ".*OPENSTACKS.*"))
-          (delay (load-and-collect-problems
-                  '(:pddl.instances.rover)
-                  '(:objective)
-                  "ROVERPROB[0-9]*"))
-          (delay (load-and-collect-problems
-                  '(:pddl.instances.satellite-typed)
-                  '(:direction)
-                  "SATELLITE-TYPED-.*"))
-          (delay (load-and-collect-problems
-                  '(:pddl.instances.woodworking-large
-                    :pddl.instances.woodworking-xlarge)
-                  '(:part)
-                  "WOOD-PROB-SAT-[0-9]*"))))
+(defparameter *delayed-problems*
+              (list (delay (load-and-collect-problems
+                            '(:pddl.instances.cell-assembly-eachparts)
+                            '(:base)
+                            "CELL-ASSEMBLY-MODEL2A-EACH-[1-5]$"))))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defmacro map-reduce (mapper reducer sequence &rest reduce-args)
