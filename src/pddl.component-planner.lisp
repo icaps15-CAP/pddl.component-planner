4057b302962fa373ad2395cc1e4f3e5a88b85be2
Masataro Asai <guicho2.71828@gmail.com>
2014-03-05 03:05:11 +0900

    factor of 1000x

--- src/pddl.component-planner.lisp
+++ src/pddl.component-planner.lisp
@@ -91,7 +91,7 @@ returns a PDDL-PLAN."
                      (path *domain*)
                      :time-limit 1
                      :hard-time-limit (* 60 5)
-                     :memory (sb-ext:dynamic-space-size) ;; 15GB * 0.8 = 120
+                     :memory (floor (/ (sb-ext:dynamic-space-size) 1000)) ;; 15GB * 0.8 = 120
                      ;; :options "--search astar(lmcut())"
                      ))))
               ;; (assert (= 4 (length result)))
