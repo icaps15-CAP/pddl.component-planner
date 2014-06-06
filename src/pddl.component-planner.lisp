0df70633e83198dcb79beedf7d204029e0ea7be7
Masataro Asai <guicho2.71828@gmail.com>
2014-06-06 16:53:49 +0900

    code cleanup and reorganization

--- src/pddl.component-planner.lisp
+++ src/pddl.component-planner.lisp
@@ -77,7 +77,9 @@ Change the value of keep-objects or keep-init.")
 @export
 (defcached plan-task (task)
   "Calls build-component-problem, make a plan with FD, then parse the results.
-returns a PDDL-PLAN."
+returns a PDDL-PLAN. The call to this function is cached and memoized, so be
+careful if you measure the elapsed time. When you measure the time, run
+(clear-plan-task-cache) to clear the cache."
   (let* ((*problem* (build-component-problem task))
          (*domain* (domain *problem*)))
     (mapcar (let ((i 0))
@@ -100,45 +102,51 @@ returns a PDDL-PLAN."
 
 @export
 (defun clear-plan-task-cache ()
+  "Clear the cache for plan-task."
   (clear-cache *PLAN-TASK-CACHE*))
 
 @export
 (defun task-plan-equal (t1 t2)
+  "Computes plan-wise compatibility. It returns true if any of the component plan
+mapped from t1 to t2 is a valid plan of t2."
   ;; (assert (abstract-component-task-strict= t1 t2))
   (let ((*default-keep-init* nil)
         (*default-keep-objects* nil)
-        (*validator-verbosity* nil)
-        (first-time t))
+        (*validator-verbosity* nil))
+    (signal 'comparison-signal)
+    (let* ((problem2 (build-component-problem t2))
+           (problem-path2 (write-problem problem2)))
+      (some 
+       (lambda (plan1)
+         (validate-plan (path (domain problem2))
+                        problem-path2
+                        (write-plan
+                         (apply-mapping
+                          plan1
+                          (mapping-between-tasks t1 t2)
+                          problem2))))
+       (plan-task-with-full-restoration t1)))))
+
+(defun plan-task-with-full-restoration (t1)
+  "It first tries to compute a
+component plan of t1 and return the result plans.
+If it fails, it restore the objects in the problem."
+  (let ((first-time t))
     (handler-bind ((plan-not-found
                     (lambda (c)
                       @ignore c
                       (if first-time
-                          (progn
-                            (setf *validator-verbosity* nil
-                                  *default-keep-init* t
-                                  *default-keep-objects* t
-                                  first-time nil)
-                            (signal 'restored-evaluation-signal)
-                            (invoke-restart (find-restart 'retry)))
+                          (invoke-restart (find-restart 'retry))
                           (return-from task-plan-equal nil)))))
-      (do-restart ((retry (lambda () (warn "Retrying, restoring objects in the other tasks."))))
-        (signal 'comparison-signal)
-        (let* ((problem2
-                ;; @break+
-                (build-component-problem t2))
-               (problem-path2
-                ;; @break+
-                (write-problem problem2)))
-          (some 
-           (lambda (plan1)
-             (validate-plan
-              (path (domain problem2))
-              problem-path2
-              (write-plan
-               (apply-mapping plan1 (mapping-between-tasks t1 t2) problem2))
-              ;; :verbose t
-              ))
-           (plan-task t1)))))))
+      (do-restart ((retry
+                    (lambda ()
+                      (setf *validator-verbosity* nil
+                            *default-keep-init* t
+                            *default-keep-objects* t
+                            first-time nil)
+                      (signal 'restored-evaluation-signal)
+                      (warn "Retrying, restoring objects in the other tasks."))))
+        (plan-task t1)))))
 
 @export
 (defun fluently-connected-objects (components attributes f)
