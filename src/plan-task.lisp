b7e035b3303929284e923f05994afd4c1c492df2
Masataro Asai <guicho2.71828@gmail.com>
2014-06-07 08:50:54 +0900

    prepare for iterative restoration

--- src/plan-task.lisp
+++ src/plan-task.lisp
@@ -6,26 +6,24 @@
   "Calls build-component-problem, make a plan with FD, then parse the results.
 returns a PDDL-PLAN. The call to this function is cached and memoized, so be
 careful if you measure the elapsed time. When you measure the time, run
-(clear-plan-task-cache) to clear the cache."
+ (clear-plan-task-cache) to clear the cache."
   (let* ((*problem* (build-component-problem task))
          (*domain* (domain *problem*)))
     (mapcar (let ((i 0))
               (curry #'pddl-plan
                      :name (concatenate-symbols (name *problem*) 'plan (incf i))
                      :path))
-            (let ((result
-                   (multiple-value-list
-                    (test-problem
-                     (write-problem *problem*)
-                     (path *domain*)
-                     :time-limit 1
-                     :hard-time-limit (* 60 5)
-                     :memory (floor (/ (sb-ext:dynamic-space-size) 1000)) ;; 15GB * 0.8 = 120
-                     ;; :options "--search astar(lmcut())"
-                     ))))
-              ;; (assert (= 4 (length result)))
-              (signal 'evaluation-signal :time (cdr result))
-              (car result)))))
+            (multiple-value-match (test-problem
+                                   (write-problem *problem*)
+                                   (path *domain*)
+                                   :time-limit 1
+                                   :hard-time-limit (* 60 5)
+                                   :memory (floor (/ (sb-ext:dynamic-space-size) 1000)) ;; 15GB * 0.8 = 120
+                                   ;; :options "--search astar(lmcut())"
+                                   )
+              ((plans t-time p-time s-time t-memory p-memory s-memory)
+               (signal 'evaluation-signal :time (list t-time p-time s-time t-memory p-memory s-memory))
+               plans)))))
 
 @export
 (defun clear-plan-task-cache ()
@@ -75,3 +73,23 @@ If it fails, it restore the objects in the problem."
                       (warn "Retrying, restoring objects in the other tasks."))))
         (plan-task t1)))))
 
+;; (defun plan-task-with-iterative-restoration (t1)
+;;   "It first tries to compute a
+;; component plan of t1 and return the result plans.
+;; If it fails, it restore the objects in the problem."
+;;   (let ((first-time t))
+;;     (handler-bind ((plan-not-found
+;;                     (lambda (c)
+;;                       @ignore c
+;;                       (if first-time
+;;                           (invoke-restart (find-restart 'retry))
+;;                           (return-from plan-task-with-full-restoration nil)))))
+;;       (do-restart ((retry
+;;                     (lambda ()
+;;                       (setf *validator-verbosity* nil
+;;                             *default-keep-init* t
+;;                             *default-keep-objects* t
+;;                             first-time nil)
+;;                       (signal 'restored-evaluation-signal)
+;;                       (warn "Retrying, restoring objects in the other tasks."))))
+;;         (plan-task t1)))))
