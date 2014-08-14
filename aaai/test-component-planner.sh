52cfb05c04919383a870e8c0651f0d5c281639d9
Masataro Asai <guicho2.71828@gmail.com>
2014-08-14 20:36:03 +0900

    verbose test-component-planner.sh

--- aaai/test-component-planner.sh
+++ aaai/test-component-planner.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-for dir in $(ls -d)
-do
-    echo $dir
-    ./component-planner $dir/p01.pddl 2>&1 | tee $dir/p01.component-planner.log
-done
+TIMER="/usr/bin/time -f 'real %e\nuser %U\nsys %S\nmaxmem %M'"
+
+run(){
+    echo $1
+    log=${1%.*}.component-planner.log
+    rm -f $log
+    ulimit -v 3000000 -t 1900
+    /usr/bin/time -f 'real %e\nuser %U\nsys %S\nmaxmem %M' \
+        ./component-planner -v $1 &> $log
+    if [[ $(cat ${1%.*}.plan) != "" ]]
+    then
+        echo plan found!
+    fi
+}
+
+run elevators-sat11/p01.pddl
+
+# for problem in $(find -name "p01.pddl")
+# do
+#     run $problem
+# done
+
+# for problem in $(find -name "p04.pddl")
+# do
+#     run $problem
+# done
