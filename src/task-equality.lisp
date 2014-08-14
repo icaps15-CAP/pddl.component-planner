8f53336defbf3b341dfa297a0339270960785ff6
Masataro Asai <guicho2.71828@gmail.com>
2014-08-14 20:59:58 +0900

    plan-task-equal returns the secondary value

--- src/task-equality.lisp
+++ src/task-equality.lisp
@@ -4,24 +4,40 @@
 @export
 (defun task-plan-equal (t1 t2)
   "Computes plan-wise compatibility. It returns true if any of the component plan
-mapped from t1 to t2 is a valid plan of t2."
+mapped from t1 to t2 is a valid plan of t2.
+
+The secondary value is the component plans for t1, which might be nil due
+to the internal time limit. If the secondary value is nil, the primary
+value is also nil.
+
+This function guarantees the compatibility between the tasks. However, IT
+DOES NOT DUARANTEE the INcompatibility between the tasks. If the primary
+value is NIL, there are 3 possibilities: there are no plans of t1 found in
+the time limit, all plans of t1 was not compatible with t2, or, finally,
+only part of possible plans for t1 were found and all of them were
+incompatible. We cannot tell between the second and the third case, so this
+function basically cannot negatively prove the compatibility.
+"
   ;; (assert (abstract-component-task-strict= t1 t2))
   (signal 'comparison-signal)
   (let* ((dir (mktemp "task-equal" t))
          (problem2 (build-component-problem t2))
          (problem-path2 (write-pddl problem2 "mapped-problem.pddl" dir))
-         (domain-path (write-pddl (domain problem2) "domain.pddl" dir)))
-    (some
-     (let ((i 0))
-       (lambda (plan1)
-         (validate-plan
-          domain-path
-          problem-path2
-          (write-plan
-           (map-component-plan plan1 (mapping-between-tasks t1 t2))
-           (prog1 (format nil "mapped.plan.~a" i) (incf i))
-           dir
-           (ask-for verbose t)))))
-     (plan-task-with-retry t1))))
+         (domain-path (write-pddl (domain problem2) "domain.pddl" dir))
+         (plans (plan-task-with-retry t1)))
+    (values
+     (some
+      (let ((i 0))
+        (lambda (plan1)
+          (validate-plan
+           domain-path
+           problem-path2
+           (write-plan
+            (map-component-plan plan1 (mapping-between-tasks t1 t2))
+            (prog1 (format nil "mapped.plan.~a" i) (incf i))
+            dir
+            (ask-for verbose t)))))
+      plans)
+     plans)))
 
 
