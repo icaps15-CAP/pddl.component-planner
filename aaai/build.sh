a4f89ba5406409aa87946688ec7f051a91727800
Masataro Asai <guicho2.71828@gmail.com>
2014-08-18 02:33:36 +0900

    turn on the interpret-pddl flag

--- aaai/build.sh
+++ aaai/build.sh
@@ -4,9 +4,11 @@
 
 sbcl --dynamic-space-size 2500 \
     --disable-debugger \
+    --eval '(push :interpret-pddl *features*)' \
     --load main.lisp 
 
 sbcl --dynamic-space-size 2500 \
     --disable-debugger \
+    --eval '(push :interpret-pddl *features*)' \
     --eval '(push :add-cost *features*)' \
     --load main.lisp
