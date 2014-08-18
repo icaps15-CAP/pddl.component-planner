acea203e2e4e95b4410499b95f2a0d8ccde37961
Masataro Asai <guicho2.71828@gmail.com>
2014-08-18 13:27:19 +0900

    merged into one makefile

--- aaai/copy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./build.sh
-cp -fv component-planner* ~/repos/component-planner/
-chmod +x  ~/repos/component-planner/component-planner*
--- aaai/refresh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sbcl --quit \
-    --eval "(ql:register-local-projects)" \
-    --eval '(push :interpret-pddl *features*)' \
-    --eval '(ql:quickload :pddl.component-planner.experiment)'
-
-
