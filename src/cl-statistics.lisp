1ae7085ae45b294ad653ae1e319b293f3a8c5211
Masataro Asai <guicho2.71828@gmail.com>
2014-08-20 20:46:22 +0900

    avoid error in length=1 cases

--- src/cl-statistics.lisp
+++ src/cl-statistics.lisp
@@ -308,10 +308,12 @@
 
 (defun standard-deviation (sequence)
   (test-variables (sequence :numseq))
-  (let ((mean (mean sequence))
-        (n (length sequence)))
-    (sqrt (/ (reduce #'+ (map 'list #'(lambda (x) (square (- mean x))) sequence))
-             (1- n)))))
+  (let ((n (length sequence)))
+    (if (= n 1)
+        0.0d0
+        (let ((mean (mean sequence)))
+          (sqrt (/ (reduce #'+ (map 'list #'(lambda (x) (square (- mean x))) sequence))
+                   (1- n)))))))
 
 
 (defun sd (sequence)
