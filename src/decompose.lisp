bb547ea7b2382099a00e29e8741b2e2d5ef1b669
Masataro Asai <guicho2.71828@gmail.com>
2014-08-23 16:21:28 +0900

    added normalized-score filtering method

--- src/decompose.lisp
+++ src/decompose.lisp
@@ -80,6 +80,112 @@
   (format t "~&Filtering null macros.")
   (remove-if (lambda-match ((vector _ nil) t)) pairs))
 
+;;;;; normalized score
+
+(defun basedata (pair)
+  (ematch pair
+    ((vector (and bag (list* (abstract-component-task-
+                              (ac (abstract-component components))) _))
+             (macro-action actions))
+     (vector (length bag)
+             (length components)
+             (length actions)))))
+
+
+(defun map-column (data fn index)
+  (flet ((v (datum) (aref datum index)))
+    (funcall fn (map 'vector #'v data))))
+
+(defun norminfo (data)
+  (iter (for i below 3)
+        (collect
+            (vector (map-column data #'mean i)
+                    (map-column data #'standard-deviation i)))))
+(defun normalize (mean stdev x)
+  (/ (- x mean) (if (zerop stdev) 1 stdev)))
+(defun normalize-data (data)
+  (let ((norminfo (norminfo data)))
+    (map 'vector
+         (lambda (datum)
+           (iter (for x in-vector datum)
+                 (for v in norminfo)
+                 (ematch v
+                   ((vector mean stdev)
+                    (collect (normalize mean stdev x)
+                      result-type vector)))))
+         data)))
+
+(defun ^2 (x) (* x x))
+
+(defvar *weights* (vector 1.0 0.5 0.1))
+(defun ms-diff-score (d)
+  (- (mean d) (standard-deviation d)))
+(defun ms-rate-score (d)
+  (let ((stdev (standard-deviation d)))
+    (if (zerop stdev)
+        (if (plusp (reduce #'+ d))
+            MOST-POSITIVE-SINGLE-FLOAT
+            MOST-NEGATIVE-SINGLE-FLOAT)
+        (/ (mean d) stdev))))
+
+(defun score (datum)
+  (ms-diff-score
+   (map 'vector #'* datum *weights*)))
+
+(defun filter-macros-normalized (pairs)
+  (when pairs
+    (let* ((data (map 'vector #'basedata pairs))
+           (ndata (normalize-data data))
+           (scores (map 'vector #'score ndata))
+           (threshold (+ (mean scores)
+                         (* (standard-deviation scores) *threshold*)))
+           (all (iter (for pair in pairs)
+                      (for datum in-vector data)
+                      (for ndatum in-vector ndata)
+                      (for score in-vector scores)
+                      (collect (list pair datum ndatum score))))
+           (all (sort all #'> :key #'fourth))
+           (results (remove-if-not
+                     (curry #'< threshold) all
+                     :key #'fourth)))
+      (print-results all threshold)
+      (format t "~&Pruning threshold is ~a." threshold)
+      (when (< (length results) 2)
+        (format t "~&This threshold value prunes too many macros. ~
+                     Recovering at least 2.")
+        (setf results (subseq all 0 (min 2 (length all)))))
+      (format t "~&~a macros are filtered down to ~a."
+              (length pairs) (length results))
+      (mapcar #'first results))))
+
+(defvar *sep*
+    "----------------------------------------------------------------")
+(defun print-results (results th)
+  (format t "~&~a macros, status:" (length results))
+  (when results
+    (iter (for r in results)
+          (for s = (print-result r))
+          (for ps previous s)
+          (when (and ps (< s th ps))
+            (format t "~&~a" *sep*)))))
+(defun print-result (result)
+  (ematch result
+    ((list (vector (list* (abstract-component-task-
+                           (ac (abstract-component seed))) _) _)
+           (vector bag components actions)
+           _ ; (vector nbag ncomponents nactions)
+           score)
+     (format t "~&(:tasks ~a :objs ~a :macro-length ~a ~
+                   :score ~,4f :seed ~a)"
+             ;; :tasks ~,4f :objs ~,4f :macro-length ~,4f ~
+             bag components actions
+             ;; nbag ncomponents nactions
+             score
+             (name (type seed)))
+     score)))
+
+;;;;; old implementation of fixed and normdist filtering
+
 (defun score-pair (pair)
   (ematch pair
     ((vector (and bag (list* (abstract-component-task-
@@ -108,7 +214,6 @@
   (subseq pairs 0 (min 2 (length pairs))))
 
 (defvar *threshold* (z 0.8))
-
 
 (defun sort-and-print-macros (pairs)
   (format t "~&~a macros, status:" (length pairs))
