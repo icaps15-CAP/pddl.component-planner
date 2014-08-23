75a5f4bd8d81ec171596e83ec4d7ba40d59d1266
Masataro Asai <guicho2.71828@gmail.com>
2014-08-23 12:42:05 +0900

    added testing

--- aaai/profile.lisp
+++ aaai/profile.lisp
@@ -3,6 +3,7 @@
 (ql:quickload :pddl.component-planner.experiment)
 (sb-profile:profile "PDDL" "PDDL.COMPONENT-PLANNER" "PDDL.COMPONENT-ABSTRACTION" "GUICHO-UTILITIES" "EAZYLAZY")
 (in-package "PDDL.COMPONENT-PLANNER.EXPERIMENT")
+
 (time (pddl.component-planner.experiment::test1-1))
 (time (pddl.component-planner.experiment::test1-2))
 (time (pddl.component-planner.experiment::test1-3))
@@ -19,3 +20,13 @@
 (sb-profile:report :print-no-call-list nil)
 
 (time (pddl.component-planner.experiment::test2-3))
+
+
+;; testing
+(progn
+  (time (pddl.component-planner.experiment::test1-1))
+  (time (pddl.component-planner.experiment::test2-1))
+  (time (pddl.component-planner.experiment::test3-1))
+  (time (pddl.component-planner.experiment::test4-1))
+  (time (pddl.component-planner.experiment::test5-1))
+  (time (pddl.component-planner.experiment::test6-1)))
