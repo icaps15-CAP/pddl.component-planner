f26b5b18473d4ac740a17f963b2ffcbdc7e34dc4
Masataro Asai <guicho2.71828@gmail.com>
2014-09-08 19:13:16 +0900

    updated help string

--- aaai/main.lisp
+++ aaai/main.lisp
@@ -78,6 +78,12 @@
        (main (list* "--preprocessor" "macroff-clean" "" rest)))
       ((list* "--main-search-ff" rest)
        (main (list* "--main-search" "macroff-clean" "" rest)))
+      ((list* "--use-marvin" rest)
+       (main (list* "--preprocess-marvin" "--main-search-marvin" rest)))
+      ((list* "--preprocess-marvin" rest)
+       (main (list* "--preprocessor" "marvin-clean" "" rest)))
+      ((list* "--main-search-marvin" rest)
+       (main (list* "--main-search" "marvin-clean" "" rest)))
       ;; time limit and resource
       ((list* "--preprocess-limit" time rest)
        (let ((*preprocess-time-limit* (parse-integer time)))
@@ -99,24 +105,32 @@
        (let ((ppath (merge-pathnames ppath))
              (dpath (merge-pathnames dpath)))
          (solve ppath dpath)))
-      (_
-       (format *error-output* "~&Invalid Arguments!")
+      (nil
        (format *error-output* "~&Usage: component-planner PROBLEM [DOMAIN]~
-               ~%~@{~4t~25a ~:[          ~;~:*~10a~] : ~a~%~}"
+               ~%~@{~4t~40<~a~;~{~a ~}~> : ~a~%~}"
                "-v" nil "specify verbosity"
                "--validation" nil "run the validator after the planning"
                "--preprocess-only" nil "stops immediately when preprocess finishes"
                "--disable-filtering" nil "disable ranking-based macro filtering"
-               "--use-grounded-actions" nil "use non-parametric fulkly grounded macro actions"
+               "--use-grounded-actions" nil "use non-parametric fully grounded macro actions"
+               "--preprocess-limit" '(time) "specify the approximated sum of maxmimum preprocessing time in integer"
+               "--component-plan-limit" '(time) "specify the time limit of component planning in integer"
+               "-t" '(time) "time limit for the main search. NOT the total limit"
+               "-m" '(memory) "memory limit for the main search. NOT the total limit"
+               "--------underlying planner options------" nil "-------------------------------"
+               "--main-search" '(string string) "specify the additional options given to the underlying planner."
+               "--preprocessor" '(string string) "specify the additional options given to the underlying planner."
+               "-----------------shortcuts--------------" nil "-------------------------------"
                "--preprocess-ff" nil "use FF during preprocesssing (otherwise LAMA ipc 2011)"
                "--main-search-ff" nil "use FF during main search (otherwise LAMA ipc 2011)"
                "--use-ff" nil "both --preprocess-ff and --main-search-ff"
-               "--main-options" 'string "specify the additional options given to macroff/fastdownward."
-               "--preprocess-limit" 'time "specify the approximated sum of maxmimum preprocessing time in integer"
-               "--component-plan-limit" 'time "specify the time limit of component planning in integer"
-               "-t" 'time "time limit for the main search. NOT the total limit"
-               "-m" 'memory "memory limit for the main search. NOT the total limit")
-       (format *error-output* "~&DOMAIN is by default domain.pddl in the same directory")
+               "--preprocess-marvin" nil "use MARVIN during preprocesssing (otherwise LAMA ipc 2011)"
+               "--main-search-marvin" nil "use MARVIN during main search (otherwise LAMA ipc 2011)"
+               "--use-marvin" nil "both --preprocess-marvin and --main-search-marvin")
+       (format *error-output* "~&DOMAIN is by default domain.pddl in the same directory"))
+      (_
+       (format *error-output* "~&Invalid Arguments!")
+       (main nil)
        (error "~&Invalid Arguments!~2%")))))
 
 (defun toplevel ()
