01a1ffffe75a93e1e56ff44d7179381479004537
Masataro Asai <guicho2.71828@gmail.com>
2014-11-16 08:11:19 +0900

    improvement: do not read the plan @ --plain mode

--- src/plan.lisp
+++ src/plan.lisp
@@ -19,43 +19,51 @@
             (print domain)
             (print pname)
             (print problem)
-            (let ((plans
-                   (if *use-plain-planner*
-                       (plan-plain domain problem)
+            (if *use-plain-planner*
+                (plan-plain ppath domain problem)
+                (let ((plans
                        (solve-problem-enhancing problem
                                                 :time-limit 1 ; satisficing
                                                 :name *main-search*
                                                 :options *main-options*
-                                                :verbose *verbose*))))
-              (iter (for plan in plans)
-                    (for i from 1)
-                    (for plp =
-                         (merge-pathnames
-                          (format nil "~a.plan.~a"
-                                  (pathname-name ppath) i)))
-                    (when (probe-file plp) (delete-file plp))
-                    (write-plan plan plp *default-pathname-defaults* t)
-                    (when *validation*
-                      (always
-                       (validate-plan dpath ppath plp :verbose *verbose*)))))))
+                                                :verbose *verbose*)))
+                  (iter (for plan in plans)
+                        (for i from 1)
+                        (for plp =
+                             (merge-pathnames
+                              (format nil "~a.plan.~a"
+                                      (pathname-name ppath) i)))
+                        (when (probe-file plp) (delete-file plp))
+                        (write-plan plan plp *default-pathname-defaults* t)
+                        (when *validation*
+                          (always
+                           (validate-plan dpath ppath plp :verbose *verbose*))))))))
       (format t "~&Wall time: ~a sec"
               (- (get-universal-time) *start*)))))
 
-(defun plan-plain (*domain* *problem*)
+(defun plan-plain (ppath *domain* *problem*)
   (let ((dir (mktemp "plain")))
-    (mapcar (curry #'pddl-plan
-                   :domain *domain*
-                   :problem *problem* :path)
-            (test-problem-common
-             (write-pddl (if *remove-main-problem-cost*
-                             (remove-costs *problem*)
-                             *problem*)
-                         "problem.pddl" dir)
-             (write-pddl (if *remove-main-problem-cost*
-                             (remove-costs *domain*)
-                             *domain*)
-                         "domain.pddl" dir)
-             :name *main-search*
-             :options *main-options*
-             :verbose *verbose*))))
+    (let ((plans
+           (test-problem-common
+            (write-pddl (if *remove-main-problem-cost*
+                            (remove-costs *problem*)
+                            *problem*)
+                        "problem.pddl" dir)
+            (write-pddl (if *remove-main-problem-cost*
+                            (remove-costs *domain*)
+                            *domain*)
+                        "domain.pddl" dir)
+            :name *main-search*
+            :options *main-options*
+            :verbose *verbose*)))
+      (iter (for path in plans)
+            (for i from 1)
+            (for new-path =
+                 (merge-pathnames
+                  (format nil "~a.plan.~a"
+                          (pathname-name ppath) i)))
+            (when (probe-file new-path) (delete-file new-path))
+            (sb-ext:run-program
+             "/bin/cp" (list (namestring path)
+                             (namestring new-path)))))))
 
