c99c0decfc09cf140409955bf2bbe43d8650498b
Masataro Asai <guicho2.71828@gmail.com>
2015-04-08 18:14:45 +0900

    exit status returns if the plan was found

--- src/plan.lisp
+++ src/plan.lisp
@@ -13,36 +13,38 @@
 (defvar *use-plain-planner* nil)
 @export
 (defun solve (ppath dpath)
-  (let ((*start* (get-universal-time)))
-    (unwind-protect
-        (multiple-value-bind (dname domain) (suppress (parse-file dpath nil t))
-          (multiple-value-bind (pname problem) (suppress (parse-file ppath nil t))
-            (print dname)
-            (print domain)
-            (print pname)
-            (print problem)
-            (if *use-plain-planner*
-                (plan-plain ppath domain problem)
-                (let ((plans
-                       (solve-problem-enhancing problem
-                                                :time-limit 1 ; satisficing
-                                                :name *main-search*
-                                                :options *main-options*
-                                                :verbose *verbose*
-                                                :iterated *iterated*)))
-                  (iter (for plan in plans)
-                        (for i from 1)
-                        (for plp =
-                             (merge-pathnames
-                              (format nil "~a.plan.~a"
-                                      (pathname-name ppath) i)))
-                        (when (probe-file plp) (delete-file plp))
-                        (write-plan plan plp *default-pathname-defaults* t)
-                        (when *validation*
-                          (always
-                           (validate-plan dpath ppath plp :verbose *verbose*))))))))
-      (format t "~&Wall time: ~a sec"
-              (- (get-universal-time) *start*)))))
+  (uiop:quit
+   ((lambda (x) (if x 0 1))
+    (let ((*start* (get-universal-time)))
+      (unwind-protect
+          (multiple-value-bind (dname domain) (suppress (parse-file dpath nil t))
+            (multiple-value-bind (pname problem) (suppress (parse-file ppath nil t))
+              (print dname)
+              (print domain)
+              (print pname)
+              (print problem)
+              (if *use-plain-planner*
+                  (plan-plain ppath domain problem)
+                  (let ((plans
+                         (solve-problem-enhancing problem
+                                                  :time-limit 1 ; satisficing
+                                                  :name *main-search*
+                                                  :options *main-options*
+                                                  :verbose *verbose*
+                                                  :iterated *iterated*)))
+                    (iter (for plan in plans)
+                          (for i from 1)
+                          (for plp =
+                               (merge-pathnames
+                                (format nil "~a.plan.~a"
+                                        (pathname-name ppath) i)))
+                          (when (probe-file plp) (delete-file plp))
+                          (write-plan plan plp *default-pathname-defaults* t)
+                          (when *validation*
+                            (always
+                             (validate-plan dpath ppath plp :verbose *verbose*))))))))
+        (format t "~&Wall time: ~a sec~%"
+                (- (get-universal-time) *start*)))))))
 
 @export
 (defvar *training-instances* nil)
